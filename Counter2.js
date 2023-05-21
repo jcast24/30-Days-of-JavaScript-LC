c4bf51026a5b13b0d5291b29a68eba054ac81085
Jesus <castrojesus39@gmail.com>
2023-05-20 23:09:58 -0700

    Added another version of Counter II

--- Counter2.js
+++ Counter2.js
@@ -2,22 +2,45 @@
  * @param {integer} init
  * @return { increment: Function, decrement: Function, reset: Function }
  */
+// var createCounter = function(init) {
+//     let count = init;
+
+//     return {
+//         increment: function() {
+//             return ++count; 
+//         },
+//         decrement: function() {
+//             return --count; 
+//         }, 
+//         reset: function(){
+//             count = init; 
+//             return count; 
+//         }
+//     }
+// };
 var createCounter = function(init) {
-    let count = init;
+    let count = init; 
+
+    function increment() {
+        return ++count; 
+    }
+
+    function decrement() {
+        return --count; 
+    }
+
+    function reset() {
+        count = init; 
+        return count; 
+    }
 
     return {
-        increment: function() {
-            return ++count; 
-        },
-        decrement: function() {
-            return --count; 
-        }, 
-        reset: function(){
-            count = init; 
-            return count; 
-        }
+        increment: increment,
+        decrement: decrement, 
+        reset: reset
     }
-};
+}
+
 
 /**
  * const counter = createCounter(5)
@@ -25,3 +48,8 @@ var createCounter = function(init) {
  * counter.reset(); // 5
  * counter.decrement(); // 4
  */
+
+const counter = createCounter(5)
+console.log(counter.increment()); // 6
+console.log(counter.reset());     // 5
+console.log(counter.decrement()); // 4
