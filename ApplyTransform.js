ca61f20c3adb4e6fa723e708b2a01ee40fd84b8d
Jesus <castrojesus39@gmail.com>
2023-05-20 23:30:47 -0700

    Fixed an issue

--- ApplyTransform.js
+++ ApplyTransform.js
@@ -6,7 +6,7 @@
 var map = function(arr, fn) {
     let result = [];
     for(let i=0; i<arr.length; i++){
-        result.push(fn(arr[i], i))
+        result.push(fn(arr[i]))
     }
     return result; 
 }
